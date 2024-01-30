5278ede845e1dd689d5dc10ffa9dd3016a7f1fe2
Cintia Elisa <cintiaelisa@gmail.com>
2024-01-30 16:19:38 -0300

    Vert.x Verticle Config

--- vertx-starter/src/main/java/com/cintia/elisa/verticles/VerticleN.java
+++ vertx-starter/src/main/java/com/cintia/elisa/verticles/VerticleN.java
@@ -6,7 +6,9 @@ import io.vertx.core.Promise;
 public class VerticleN extends AbstractVerticle {
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    System.out.println("Start " + getClass().getName() +  " on thread " + Thread.currentThread().getName());
+    System.out.println("Start " + getClass().getName() +
+      " on thread " + Thread.currentThread().getName() +
+      " with config " + config().toString());
     startPromise.complete();
   }
 }
