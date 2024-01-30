ef84ec46a9bcd7aaa34e321c089353ef48d045a8
Cintia Elisa <cintiaelisa@gmail.com>
2024-01-30 16:06:37 -0300

    Vert.x Verticles

---
+++ vertx-starter/src/main/java/com/cintia/elisa/verticles/VerticleA.java
@@ -0,0 +1,20 @@
+package com.cintia.elisa.verticles;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class VerticleA extends AbstractVerticle {
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    System.out.println("Start " + getClass().getName());
+    vertx.deployVerticle(new VerticleAA(), whenDeployed -> {
+      System.out.println("Deployed " + VerticleAA.class.getName());
+      vertx.undeploy(whenDeployed.result());
+    });
+    vertx.deployVerticle(new VerticleAB(), whenDeployed -> {
+      System.out.println("Deployed " + VerticleAB.class.getName());
+      // Do not undeploy
+    });
+    startPromise.complete();
+  }
+}
